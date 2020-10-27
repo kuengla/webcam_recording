b33e4f00097c8d26fab6bccd77f6da8c760dbbb7
kuengla <lars.kueng@ch.schindler.com>
2020-10-27 16:03:17 +0100

    creaty video stream reccording in python main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("Hello Worl")
+#!/usr/bin/env python
+
+"""main.py: record the stream of your Webcam. Quit with 'q'. File will be found in project folder."""
+
+__author__      = "Lars Kung"
+__version__ = "1.0.0"
+
+# pip install opencv-python
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,1)
+
+        # write the flipped frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
